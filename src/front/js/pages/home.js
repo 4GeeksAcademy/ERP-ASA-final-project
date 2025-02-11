5d3f7ab5898f6e4b5a57153b205c1e8c34379811
RGAlvaro <104777178+RGAlvaro@users.noreply.github.com>
2025-02-11 17:29:32 +0000

    Nueva correccion en home.js

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,8 +4,7 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
+	
 	return (
 	<div className="container my-5">
 		<section className="text-center text-white d-flex flex-column justify-content-center align-items-center"
