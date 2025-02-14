88e7051a931f16c0229790434738a457ad64b12c
RGAlvaro <104777178+RGAlvaro@users.noreply.github.com>
2025-02-14 18:14:35 +0000

    Eliminados estilos en linea

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
   export const Navbar = () => {
 	return (
-	  <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#2D336B" }}>
+	  <nav className="navbar navbar-expand-lg">
 		<div className="container-fluid">
 		  <a className="navbar-brand text-white fw-bold" href="#">
 			<img className="logo" src="https://placeholder.pics/images/icons/apple-icon-180x180.png" alt="Logo" />
