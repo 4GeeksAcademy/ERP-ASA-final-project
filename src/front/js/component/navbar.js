2bb93102165e77a1923366cd55acf7924b8acdd0
RGAlvaro <104777178+RGAlvaro@users.noreply.github.com>
2025-02-14 18:12:22 +0000

    corregido boton de login invisible

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,22 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+
+  export const Navbar = () => {
 	return (
 	  <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#2D336B" }}>
 		<div className="container-fluid">
-		  {/* Logo (Imagen) */}
-		  <a className="navbar-brand" href="#">
-			<img
-			  src="https://placehold.co/60x60"  // Cambia esto por la ruta de tu logo
-			  alt="Logo"
-			  style={{ height: "40px" }}
-			/>
+		  <a className="navbar-brand text-white fw-bold" href="#">
+			<img className="logo" src="https://placeholder.pics/images/icons/apple-icon-180x180.png" alt="Logo" />
 		  </a>
-  
-		  {/* Botón de menú para móviles */}
 		  <button 
-			className="navbar-toggler navbar-light" 
+			className="navbar-toggler" 
 			type="button" 
 			data-bs-toggle="collapse" 
 			data-bs-target="#navbarContent"
@@ -24,15 +18,17 @@ export const Navbar = () => {
 			aria-expanded="false" 
 			aria-label="Toggle navigation"
 		  >
-			<span className="navbar-toggler-icon color:white"></span>
-		
+			<span className="navbar-toggler-icon"></span>
 		  </button>
   
-		  {/* Contenido del Navbar */}
 		  <div className="collapse navbar-collapse justify-content-end" id="navbarContent">
-			<div className="d-flex mt-2 mt-lg-0">
-			  <button className="btn btn-outline-light me-2">Login</button>
-			  <button className="btn btn-light text-dark">Sign Up</button>
+			<div className="d-flex">
+				<Link to="/login">
+					<button className="btn btn-light text-dark me-2">Login</button>
+				</Link>
+				<Link to="/signup">
+					<button className="btn btn-light text-dark">Sign Up</button>
+				</Link>
 			</div>
 		  </div>
 		</div>
