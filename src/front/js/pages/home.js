30680a0fdb761ea2a4b7e127669d51fd3bfebbac
RGAlvaro <104777178+RGAlvaro@users.noreply.github.com>
2025-02-14 17:36:34 +0000

    eliminados estilos en linea

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,35 +7,35 @@ export const Home = () => {
 	
 	return (
 	<div className="container my-5">
-		<section className="text-center text-white d-flex flex-column justify-content-center align-items-center"
-			style={{ backgroundColor: "#7886C7", height: "90vh", padding: "50px" }}>
+		<section className="text-center text-white d-flex flex-column justify-content-center 
+		align-items-center medium-background center-column">
 			<h1 className="fw-bold">Optimiza la Gestión de Recursos Humanos</h1>
-			<p className="mt-3" style={{ maxWidth: "600px" }}>
+			<p className="mt-3 section">
 				Automatiza procesos, mejora la eficiencia y toma mejores decisiones con nuestro módulo de RRHH integrado en tu ERP.
 			</p>
 			<button className="btn btn-light text-dark mt-3">Solicitar Demo</button>
 		</section>
 		<section className="container py-5">
-			<h2 className="text-center" style={{ color: "#2D336B" }}>¿Por qué elegir nuestro módulo?</h2>
+			<h2 className="text-center strong-background">¿Por qué elegir nuestro módulo?</h2>
 			<div className="row mt-4">
 				<div className="col-md-4 text-center">
-					<i className="bi bi-people-fill" style={{ fontSize: "40px", color: "#2D336B" }}></i>
+					<i className="bi bi-people-fill feat strong-background"></i>
 					<h4>Gestión de empleados</h4>
 					<p>Administra perfiles, roles y permisos de empleados de forma sencilla.</p>
 				</div>
 				<div className="col-md-4 text-center">
-					<i className="bi bi-clock-history" style={{ fontSize: "40px", color: "#2D336B" }}></i>
+					<i className="bi bi-clock-history feat strong-background"></i>
 					<h4>Control de asistencia</h4>
 					<p>Registra horarios, ausencias y vacaciones de manera automática.</p>
 				</div>
 				<div className="col-md-4 text-center">
-					<i className="bi bi-bar-chart-line" style={{ fontSize: "40px", color: "#2D336B" }}></i>
+					<i className="bi bi-bar-chart-line feat strong-background"></i>
 					<h4>Reportes inteligentes</h4>
 					<p>Obtén informes detallados para tomar decisiones estratégicas.</p>
 				</div>
 			</div>
     	</section>
-		<section className="py-5" style={{ backgroundColor: "#A9B5DF" }}>
+		<section className="py-5 light-background">
 			<h2 className="text-center text-dark">Lo que dicen nuestros clientes</h2>
 			<div className="container mt-4">
 				<div className="row">
