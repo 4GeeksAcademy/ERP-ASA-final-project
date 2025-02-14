1dfcc994f086620629d4b32dd0f43aa424b5446a
Alejandro Garrocho Cruz <101363464+AlejandroGC19@users.noreply.github.com>
2025-02-14 15:18:49 +0000

    login endpoint and jwt done

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -4,6 +4,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			messageee: "hola",
 			employeesList: [],
 			selected: [],
+			auth: false
 
 			// Ejemplo completo data:
 			// employeesList: [
@@ -66,32 +67,82 @@ const getState = ({ getStore, getActions, setStore }) => {
 				console.log(getStore().selected);
 
 			},
-			getMessage: async () => {
-				try {
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				} catch (error) {
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			login: async (email, password) => {
+				const myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+				const raw = JSON.stringify({
+					"email": email,
+					"password": password
 				});
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+				const requestOptions = {
+					method: "POST",
+					headers: myHeaders,
+					body: raw,
+					redirect: "follow"
+				};
+
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/api/login", requestOptions);
+					const result = await response.json();
+					
+					if (response.status === 200) {
+						setStore({auth: true})
+						localStorage.setItem("token", result.access_token)
+						console.log(result.access_token);
+						return true;
+					}
+				} catch (error) {
+					console.log(false);
+					return false;
+				};
+			},
+			getProfile: async () => {
+				let token = localStorage.getItem("token")
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/api/profile", {
+						method: "GET",
+						headers: {
+							"Authorization": `Bearer ${token}`
+						},
+					});
+					if (response.status === 200) {
+						const result = await response.json();
+						setStore({data: result})
+						return true;
+					}
+				} catch (error) {
+					console.error(error);
+					return false
+				};
+			},
+			// getMessage: async () => {
+			// 	try {
+			// 		// fetching data from the backend
+			// 		const resp = await fetch(process.env.BACKEND_URL + "api/hello")
+			// 		const data = await resp.json()
+			// 		setStore({ message: data.message })
+			// 		// don't forget to return something, that is how the async resolves
+			// 		return data;
+			// 	} catch (error) {
+			// 		console.log("Error loading message from backend", error)
+			// 	}
+			// },
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
 		}
 	};
 };
