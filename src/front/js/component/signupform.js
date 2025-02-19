68425105d3cdefee17db7d8cf7c46a8d9f495e22
Sergi-03 <robrovskyyt@gmail.com>
2025-02-19 19:15:42 +0000

    debug

--- src/front/js/component/signupform.js
+++ src/front/js/component/signupform.js
@@ -1,53 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import {useState, useContext, useEffect} from "react";
+import { useContext, useEffect} from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 
 
 export const SignupForm = () => {
 
-	const [name, setName] = useState("")
-    const [lastname, setLastname] = useState("")
-    const [dni, setDni] = useState("")
-    const [address, setAddress] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [birthdate, setBirthdate] = useState("")
-    const [departmentId, setDepartmentId] = useState("")
-    const [salaryId, setSalaryId] = useState("")
-    const [roleId, setRoleId] = useState("")
     const {store,actions} = useContext(Context)
-
     let navigate = useNavigate();
 
 	useEffect(()=>{
 		if (!store.auth) navigate("/")
 		actions.fetchData();
 	}, [])
-
-    const handleSubmit = async (e) => {
-		e.preventDefault();
-		const success = await actions.addWorker(name, last_name, dni, address, email, password, birthdate, parseInt(department_id), parseInt(salary_id), parseInt(role_id));
-		if (success) {
-			navigate("/profile");
-		} else {
-			alert("Error adding worker");
-		}
-	};
-        
-    
+ 
 	return (
 		<>
 		  <div className="form-container-2">
 			<p className="form-title-2">Sign up on our ERP</p>
 			<Formik
 			  initialValues={{
+				name: "",
+				lastname: "",
 				dni: "",
+				address: "",
+				email: "",
+				departmentId: "",
+				salaryId: "",
+				roleId: "",
 				birthDate: "",
-				password: "",
-				salary: ""
+				password: ""
 			  }}
 			  validate={(values) => {
 				const errors = {};
@@ -56,7 +40,7 @@ export const SignupForm = () => {
 				  errors.dni = "DNI must have at least 8 numbers.";
 				}
 
-				if (!/^\d+(\.\d{1,2})?$/.test(values.salary)) {
+				if (!/^\d+(\.\d{1,2})?$/.test(values.salaryId)) {
 					errors.salary = "Salary must be a valid number.";
 				  }
 
@@ -71,57 +55,76 @@ export const SignupForm = () => {
 				return errors;
 			  }}
 			  
-			  onSubmit={(values) => {
-				console.log("Form Sent", values);
-			  }}
-			>
-			  <Form className="form-2" onSubmit={handleSubmit}>
+			  onSubmit={async (values) => {
+				console.log("hola");
+				
+				// const success = await actions.addWorker(
+				// 	values.name,
+				// 	values.lastname,
+				// 	values.dni,
+				// 	values.address,
+				// 	values.email,
+				// 	values.departmentId,
+				// 	values.salaryId,
+				// 	values.roleId,
+				// 	values.birthDate,
+				// 	values.password
+				// )
+				// if (success) {
+				// 	navigate("/profile")
+				// }
+				// else{
+                //  alert("Error adding worker")
+				// }
+			  }} 
+
+			  >
+
+			  <Form className="form-2">
 				<div className="input-container-2">
-				  <Field placeholder="Name" name="name" type="text" onChange={(e)=>setName(e.target.value)}/>
+				  <Field placeholder="Name" name="name" type="text" />
 				</div>
 				<div className="input-container-2">
-				  <Field placeholder="Last name" name="lastName" type="text" onChange={(e)=>setLastname(e.target.value)} />
+				  <Field placeholder="Last name" name="lastname" type="text"  />
 				</div>
 				<div className="input-container-2">
-				  <Field placeholder="Dni" name="dni" type="text" onChange={(e)=>setDni(e.target.value)} />
+				  <Field placeholder="Dni" name="dni" type="text"  />
 				  <div className="error-container">
 					<ErrorMessage name="dni" component="p" className="error" />
 				  </div>
 				</div>
 				<div className="input-container-2">
-				  <Field placeholder="Address" name="address" type="text"  onChange={(e)=>setAddress(e.target.value)} />
+				  <Field placeholder="Address" name="address" type="text"  />
 				</div>
 				<div className="input-container-2">
-					<select onChange={(e) => setSalaryId(e.target.value)}>
+				<Field as="select" name="salaryId">
 						<option value="">Select Salary</option>
 						{store.salaries.map((salary) => (
 							<option key={salary.id} value={salary.id}>{salary.gross_salary}</option>
 						))}
-					</select>
+						</Field>
 					<span><i className="fa-light fa-dollar-sign"></i></span>
 				</div>
-
 				<div className="input-container-2">
-					<select onChange={(e) => setRoleId(e.target.value)}>
+				<Field as="select" name="roleId">
 						<option value="">Select Role</option>
 						{store.roles.map((role) => (
 							<option key={role.id} value={role.id}>{role.name}</option>
 						))}
-					</select>
+						</Field>
 				</div>
-
 				<div className="input-container-2">
-					<select onChange={(e) => setDepartmentId(e.target.value)}>
+				<Field as="select" name="departmentId">
 						<option value="">Select Department</option>
 						{store.departments.map((department) => (
 							<option key={department.id} value={department.id}>{department.name}</option>
 						))}
-					</select>
+						</Field>
 					<span><i className="fa-regular fa-building"></i></span>
 				</div>
 				<h5>Birth date</h5>
 				<div className="input-container-2">
-				  <Field placeholder="00/00/0000" name="birthDate" type="text" onChange={(e)=>setBirthdate(e.target.value)}/>
+				  <Field placeholder="00/00/0000" name="birthDate" type="text"/>
 				  <div className="error-container">
 					<ErrorMessage name="birthDate" component="p" className="error" />
 				  </div>
@@ -130,7 +133,7 @@ export const SignupForm = () => {
 				  </span>
 				</div>
 				<div className="input-container-2">
-				  <Field placeholder="Email" name="email" type="email"  onChange={(e)=>setEmail(e.target.value)}/>
+				  <Field placeholder="Email" name="email" type="email"  />
 				  <span>
 					<svg stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					  <path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round"></path>
@@ -138,7 +141,7 @@ export const SignupForm = () => {
 				  </span>
 				</div>
 				<div className="input-container-2">
-				  <Field placeholder="Password" name="password" type="password" onChange={(e)=>setPassword(e.target.value)}/>
+				  <Field placeholder="Password" name="password" type="password" />
 				  <div className="error-container">
 					<ErrorMessage name="password" component="p" className="error" />
 				  </div>
