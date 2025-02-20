c09c355ebc648b9a43bad3416fea5f2d005658c8
RGAlvaro <104777178+RGAlvaro@users.noreply.github.com>
2025-02-20 16:13:18 +0000

    Corregido panel de admin para que en el desplegable aparezcan nombres en lugar de IDs

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,6 +16,20 @@ class WorkerView(ModelView):
     column_sortable_list = ('id', 'name', 'last_name', 'dni', ('department_name', 'department.name'), 
                             ('role_name', 'role.name'), ('gross_salary', 'salary.gross_salary'))
 
+    form_args = {
+        'department': {
+            'query_factory': lambda: Department.query.all(),
+            'get_label': 'name'  # Muestra el nombre del departamento en el desplegable
+        },
+        'role': {
+            'query_factory': lambda: Role.query.all(),
+            'get_label': 'name'  # Muestra el nombre del rol
+        },
+        'salary': {
+            'query_factory': lambda: Salary.query.all(),
+            'get_label': 'gross_salary'  # Muestra el salario bruto
+        }
+    }
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
